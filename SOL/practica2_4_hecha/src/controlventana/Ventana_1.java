7b8ebbbc4c3a1c4eae1763e9b800ec9ced31506a
Kevin2001mol <kevin2001molina@gmail.com>
2024-10-08 09:45:22 +0200

    todos los botones puestos

---
+++ SOL/practica2_4_hecha/src/controlventana/Ventana_1.java
@@ -0,0 +1,319 @@
+
+package controlventana;
+
+
+public class Ventana_1 extends javax.swing.JFrame {
+
+ 
+    public Ventana_1() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        opcion1 = new javax.swing.JRadioButton();
+        opcion2 = new javax.swing.JRadioButton();
+        opcion3 = new javax.swing.JRadioButton();
+        opcion4 = new javax.swing.JCheckBox();
+        opcion5 = new javax.swing.JCheckBox();
+        opcion6 = new javax.swing.JCheckBox();
+        texto_correo = new javax.swing.JLabel();
+        texto_random = new javax.swing.JTextField();
+        escribir_correo = new javax.swing.JTextField();
+        boton_iphone = new javax.swing.JToggleButton();
+        combobox1 = new javax.swing.JComboBox<>();
+        spinner1 = new javax.swing.JSpinner();
+        volumen = new javax.swing.JSlider();
+        jSeparator1 = new javax.swing.JSeparator();
+        opcion1_clone = new javax.swing.JRadioButton();
+        opcion2_clone = new javax.swing.JRadioButton();
+        opcion3_clone = new javax.swing.JRadioButton();
+        opcion4_clone = new javax.swing.JCheckBox();
+        opcion5_clone = new javax.swing.JCheckBox();
+        opcion6_clone = new javax.swing.JCheckBox();
+        texto_random_clone = new javax.swing.JTextField();
+        combobox1_clone = new javax.swing.JComboBox<>();
+        spinner1_clone = new javax.swing.JSpinner();
+        volumen_clone = new javax.swing.JSlider();
+        boton_iphone_clone = new javax.swing.JToggleButton();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        opcion1.setText("Opcion 1");
+
+        opcion2.setText("Opcion 2");
+
+        opcion3.setText("Opcion 3");
+
+        opcion4.setText("Opcion 4");
+
+        opcion5.setText("Opcion 5");
+
+        opcion6.setText("Opcion 6");
+
+        texto_correo.setText("Correo");
+
+        texto_random.setText("jTextField1");
+
+        escribir_correo.setText("jTextField2");
+
+        boton_iphone.setText("jToggleButton1");
+        boton_iphone.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                boton_iphoneActionPerformed(evt);
+            }
+        });
+
+        combobox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        opcion1_clone.setText("Opcion 1");
+        opcion1_clone.setEnabled(false);
+
+        opcion2_clone.setText("Opcion 2");
+        opcion2_clone.setEnabled(false);
+
+        opcion3_clone.setText("Opcion 3");
+        opcion3_clone.setEnabled(false);
+
+        opcion4_clone.setText("Opcion 4");
+        opcion4_clone.setEnabled(false);
+
+        opcion5_clone.setText("Opcion 5");
+        opcion5_clone.setEnabled(false);
+
+        opcion6_clone.setText("Opcion 6");
+        opcion6_clone.setEnabled(false);
+
+        texto_random_clone.setText("jTextField1");
+        texto_random_clone.setEnabled(false);
+
+        combobox1_clone.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        combobox1_clone.setEnabled(false);
+
+        spinner1_clone.setEnabled(false);
+
+        volumen_clone.setEnabled(false);
+
+        boton_iphone_clone.setText("jToggleButton1");
+        boton_iphone_clone.setEnabled(false);
+
+        jLabel1.setText("Correo");
+        jLabel1.setEnabled(false);
+
+        jTextField2.setText("jTextField2");
+        jTextField2.setEnabled(false);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(opcion1_clone)
+                        .addGap(18, 18, 18)
+                        .addComponent(opcion4_clone))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(opcion2_clone)
+                        .addGap(18, 18, 18)
+                        .addComponent(opcion5_clone))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(opcion3_clone)
+                        .addGap(18, 18, 18)
+                        .addComponent(opcion6_clone)))
+                .addGap(33, 33, 33)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(combobox1_clone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(texto_random_clone, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
+                        .addComponent(volumen_clone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(38, 38, 38))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(spinner1_clone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(boton_iphone_clone)
+                        .addGap(73, 73, 73))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(opcion3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(opcion6))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(opcion2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(opcion5))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(opcion1)
+                                .addGap(28, 28, 28)
+                                .addComponent(opcion4)))
+                        .addGap(66, 66, 66)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(spinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(boton_iphone)
+                                .addGap(70, 70, 70))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(combobox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addComponent(texto_random))
+                                .addGap(18, 18, 18)
+                                .addComponent(volumen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(33, 33, 33))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(8, 8, 8)
+                        .addComponent(texto_correo, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(escribir_correo, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jSeparator1)
+                        .addContainerGap())
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(33, 33, 33)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(opcion1)
+                            .addComponent(opcion4)
+                            .addComponent(texto_random, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(opcion2)
+                            .addComponent(opcion5)
+                            .addComponent(combobox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(volumen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(opcion3)
+                    .addComponent(opcion6)
+                    .addComponent(spinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(boton_iphone))
+                .addGap(21, 21, 21)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(texto_correo)
+                    .addComponent(escribir_correo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(47, 47, 47)
+                                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(opcion1_clone)
+                                    .addComponent(opcion4_clone)
+                                    .addComponent(texto_random_clone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(opcion2_clone)
+                                    .addComponent(opcion5_clone)
+                                    .addComponent(combobox1_clone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(84, 84, 84)
+                                .addComponent(volumen_clone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(opcion3_clone)
+                            .addComponent(opcion6_clone)
+                            .addComponent(spinner1_clone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(26, 26, 26)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(108, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(boton_iphone_clone)
+                        .addGap(166, 166, 166))))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void boton_iphoneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boton_iphoneActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_boton_iphoneActionPerformed
+
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Ventana_1().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton boton_iphone;
+    private javax.swing.JToggleButton boton_iphone_clone;
+    private javax.swing.JComboBox<String> combobox1;
+    private javax.swing.JComboBox<String> combobox1_clone;
+    private javax.swing.JTextField escribir_correo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JRadioButton opcion1;
+    private javax.swing.JRadioButton opcion1_clone;
+    private javax.swing.JRadioButton opcion2;
+    private javax.swing.JRadioButton opcion2_clone;
+    private javax.swing.JRadioButton opcion3;
+    private javax.swing.JRadioButton opcion3_clone;
+    private javax.swing.JCheckBox opcion4;
+    private javax.swing.JCheckBox opcion4_clone;
+    private javax.swing.JCheckBox opcion5;
+    private javax.swing.JCheckBox opcion5_clone;
+    private javax.swing.JCheckBox opcion6;
+    private javax.swing.JCheckBox opcion6_clone;
+    private javax.swing.JSpinner spinner1;
+    private javax.swing.JSpinner spinner1_clone;
+    private javax.swing.JLabel texto_correo;
+    private javax.swing.JTextField texto_random;
+    private javax.swing.JTextField texto_random_clone;
+    private javax.swing.JSlider volumen;
+    private javax.swing.JSlider volumen_clone;
+    // End of variables declaration//GEN-END:variables
+}
